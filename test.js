4ae430c75f2d05f3f8c6460fe995797aa91deef0
BananaBird <kaimro1870@gmail.com>
2026-05-30 14:10:39 +0630

    Add ticket project

--- test.js
+++ test.js
@@ -7,3 +7,22 @@ if (num1 === 2){num2 += 3}
 else if (num1 < 2){num2 += 4}
 else if (num1 > 1){num2 += 5}
 else {num2 += 6}
+/*
+Airline ticket example
+
+not member = full price
+normalMember = 5% discount
+silverMember = 10% discount
+goldMember = 15% discount
+platinumMember = 20% discount
+*/
+
+const originalPrice = 100;
+let totalPrice = 0;
+let memberType = "silverMember"
+
+if (memberType === "normalMember"){totalPrice += 100 * 0.95;}
+else if (memberType === "silverMember"){totalPrice += 100 * 0.9;}
+else if (memberType === "goldMember"){totalPrice += 100 * 0.85;}
+else if (memberType === "platinumMember"){totalPrice += 100 * 0.8;}
+else {totalPrice = originalPrice;}
